6486267cd333c94e98e5f5099aa8d6404cb6e06c
alexn34-n <alexn3469@mail.ru>
2021-04-24 08:27:42 +0300

    Выполнена  домашняя работа к 2 уроку

--- src/main/java/ru/alexn/springcourse/TestBean.java
+++ src/main/java/ru/alexn/springcourse/TestBean.java
@@ -1,17 +1,17 @@
 package ru.alexn.springcourse;
 
 public class TestBean {
-    private String name;
+  //  private String name;
 
-    public TestBean(String name) {
-        this.name = name;
-    }
+  //  public TestBean(String name) {
+  //      this.name = name;
+  //  }
 
-    public String getName() {
-        return name;
-    }
+ //   public String getName() {
+  //      return name;
+   // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  //  public void setName(String name) {
+   //     this.name = name;
+   // }
 }
