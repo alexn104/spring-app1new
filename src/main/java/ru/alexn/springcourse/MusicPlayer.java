0167f07a746943530aee2d28ed43c86f1bd77b0c
alexn34-n <alexn3469@mail.ru>
2021-04-24 09:38:21 +0300

    Выполнена  домашняя работа к 4 уроку(setter)

--- src/main/java/ru/alexn/springcourse/MusicPlayer.java
+++ src/main/java/ru/alexn/springcourse/MusicPlayer.java
@@ -6,7 +6,15 @@ public class MusicPlayer {
     public MusicPlayer(Music music) {
         this.music = music;
     }
+    public MusicPlayer(){}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public  void  playMusic() {
         System.out.println("Playing:"+music.getSong());
     }
+
+
 }
